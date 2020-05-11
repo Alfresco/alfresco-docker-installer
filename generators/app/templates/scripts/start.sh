61cd4ce69452492b378e42623e58872a474bf839
Martin Müller <damadden88@googlemail.com>
2020-05-11 14:21:57 +0200

    add let's encrypt option after ssl port

--- generators/app/templates/scripts/start.sh
+++ generators/app/templates/scripts/start.sh
@@ -26,6 +26,7 @@ set_wait_time(){
 WAIT_TIME=500
 SERVER_NAME="<%=serverName%>"
 SERVER_PORT="<%=port%>"
+PROTOCOL="http<% if (https == 'true') { %>s<%}%>"
 
 while [[ $1 == -* ]]; do
   case "$1" in
@@ -43,7 +44,7 @@ docker-compose up -d --build
 if [[ $WAIT_TIME -gt 0 ]]; then
   echo "Waiting for alfresco to boot ..."
   WAIT_TIME=$(( ${WAIT_TIME} * 1000 ))
-  node_modules/wait-on/bin/wait-on "http://${SERVER_NAME}:${SERVER_PORT}/alfresco/" -t "${WAIT_TIME}" -i 10000 -v
+  node_modules/wait-on/bin/wait-on "${PROTOCOL}://${SERVER_NAME}:${SERVER_PORT}/alfresco/" -t "${WAIT_TIME}" -i 10000 -v
   if [ $? == 1 ]; then
     echo "Waiting failed -> exit 1"
     exit 1
