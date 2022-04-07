873779fbfeac06a269f4788535b5c5796de5f535
Douglas C. R. Paes <dpaes@dgcloud.com.br>
2022-04-07 11:12:51 -0300

    Changes in the mkdir command

--- generators/app/templates/scripts/create_volumes.sh
+++ generators/app/templates/scripts/create_volumes.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-mkdir ./data
-mkdir ./data/solr-data
+mkdir -p ./data/solr-data
 chown 33007 ./data/solr-data
 
-mkdir ./data/postgres-data
+mkdir -p ./data/postgres-data
 chown 999 ./data/postgres-data
 
-mkdir ./logs
-mkdir logs/postgres
+mkdir -p ./logs/postgres
 chown 999 logs/postgres
